47eb23f9bec173459d59dc6248e4e7caa24ed870
Hugo Iván Rodríguez Vera <hi.rodriguez1998@gmail.com>
2020-03-14 20:23:52 -0300

    Order by en operaciones de conjunto

---
+++ order_by.sql
@@ -0,0 +1,11 @@
+--=============================================================--
+SELECT employee_id,
+       job_id,
+       salary
+FROM employees
+UNION
+SELECT employee_id,
+       job_id,
+       0
+FROM job_history
+ORDER BY salary;
